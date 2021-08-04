47df346fdd7aae20491a6e5330c1809e43505630
mts-almeida <mateusoliveiraalmeida@gmail.com>
2021-08-04 20:17:17 -0300

    refactoring validate cpf

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,52 @@
-export function validate(str: string) {
+const CPF_LENGTH = 11;
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
 
-	if (str !== null && str !== undefined) {
-		if (str.length >= 11 || str.length <= 14){  
-			str=str.replace('.','').replace('.','').replace('-','').replace(" ","");  
-			if (!str.split("").every(c => c === str[0])) {
-				try{  
-					let     d1, d2;  
-					let     dg1, dg2, rest;  
-					let     digito;  
-						let     nDigResult;  
-					d1 = d2 = 0;  
-					dg1 = dg2 = rest = 0;  
-						
-					for (let nCount = 1; nCount < str.length -1; nCount++) {  
-						// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-						// 	return false;
-						// } else {
+function clearCPF(cpf: string) {
+  return cpf
+    .replace(".", "")
+    .replace(".", "")
+    .replace("-", "")
+    .replace(" ", "");
+}
 
-							digito = parseInt(str.substring(nCount -1, nCount));  							
-							d1 = d1 + ( 11 - nCount ) * digito;  
-							d2 = d2 + ( 12 - nCount ) * digito;  
-						// }
-					};  
-						
-					rest = (d1 % 11);  
-					dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-					d2 += 2 * dg1;  
-					rest = (d2 % 11);  
-					if (rest < 2)  
-						dg2 = 0;  
-					else  
-						dg2 = 11 - rest;  
-					let nDigVerific = str.substring(str.length-2, str.length);  
-					nDigResult = "" + dg1 + "" + dg2;  
-					return nDigVerific == nDigResult;
-				}catch (e){  
-					console.error("Erro !"+e);  
-					return false;  
-				}  
-			} else return false
-		}else return false;
-	} else return false;
+function numbersAreAllEquals(cpf: string) {
+	const [digit1] = cpf
+  return cpf.split("").every((digit) => digit === digit1);
+}
+
+function isInvalidLength(cpf: string){
+	return cpf.length !== CPF_LENGTH
+}
+
+function calculateDigit(cpf: string, factor: number, max: number){
+	let total = 0;
+	for(const digit of cpf.split("").slice(0,max)){
+		total += parseInt(digit) * factor--; 
+	}
+	const rest = total%11;
+	const digit = (rest<2) ? 0 : 11 - rest;
+	return digit;
+}
+
+function extractCheckerDigit(cpf: string){
+	return 	cpf.slice(9);
+}
+
+export function validate(cpf: string) {
+  if (!cpf) {
+    return false;
+  }
+  cpf = clearCPF(cpf);
+  if (isInvalidLength(cpf)) {
+    return false;
+  }
+  if (numbersAreAllEquals(cpf)) {
+    return false;
+  }
+	const dg1 = calculateDigit(cpf, FACTOR_DIGIT_1, 9);
+	const dg2 = calculateDigit(cpf, FACTOR_DIGIT_2, 10);
+	let checkerDigit = extractCheckerDigit(cpf);
+  let nDigResult = `${dg1}${dg2}`;
+  return checkerDigit == nDigResult;
 }
